f212ee715871746b40850aab24546adacb6385fc
Connor <Clad8895@proton.me>
2025-10-13 08:14:02 -0400

    Adding updates

---
+++ Automation/automation_menu.py
@@ -0,0 +1,4 @@
+def com_menu():
+    print("[1] Option #1 -  Read VMWare Username and vCenter Hostname from file")
+    print("[2] Option #2 - Information about this Pyvmomi session")
+    print("[3] Option #3 - Search for VMs")
